8b8c3585231d09bcb532c240409fabbcff0b16bf
wilberli <tu_correo@example.com>
2024-11-27 06:42:50 -0400

    cambioos

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import { routes } from './router'
 const app = createApp(App)
+app.use(PrimeVue)
 
 app.use(createPinia())
 app.use(routes)
